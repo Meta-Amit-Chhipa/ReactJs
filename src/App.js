1903ebe8deb74bde9c234b08d6e736aa552afa54
Meta-Amit-Chhipa <99795085+Meta-Amit-Chhipa@users.noreply.github.com>
2022-04-08 11:01:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FragmentFile from './FragmentDemo/FragmentFile';
+
+function App() {
+  return (
+    <div className="App">
+      <FragmentFile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FragmentDemo/Column.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Column(props) {
+    return (
+        <React.Fragment>
+            <td>Amit</td>
+            <td>Chhipa</td>
+        </React.Fragment>
+    );
+}
+
+export default Column;
