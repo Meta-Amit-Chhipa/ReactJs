a7f45dfce1c45004825e35aaed17f6655faf2327
Meta-Amit-Chhipa <99795085+Meta-Amit-Chhipa@users.noreply.github.com>
2022-04-11 11:28:44 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,34 @@
 import React  from "react";
 import Blogs from "./Blogs";
 
+
 function Post(props){
+  let item=[
+    {
+       id:"01",
+       title:'Post Title 1',
+       desc:'Post Description 1'
+    },
+    {
+      id:"02",
+      title:'Post Title 2',
+      desc:'Post Description 2'
+   },
+   {
+    id:"03",
+    title:'Post Title 3',
+    desc:'Post Description 3'
+ },
+ {
+  id:"04",
+  title:'Post Title 4',
+  desc:'Post Description 4'
+},
+  ]
       return(
           <div className="container ItemBox">
              <h5>Blogs Post</h5>
-              <Blogs
-                 title={props.item[0].title}
-                 desc={props.item[0].desc}>
-              </Blogs>
-              <Blogs
-                 title={props.item[1].title}
-                 desc={props.item[1].desc}>
-              </Blogs>
-              <Blogs
-                 title={props.item[2].title}
-                 desc={props.item[2].desc}>
-              </Blogs>
-              <Blogs
-                 title={props.item[3].title}
-                 desc={props.item[3].desc}>
-              </Blogs>
+             <Blogs item={item}/>
           </div>
       )
 }
