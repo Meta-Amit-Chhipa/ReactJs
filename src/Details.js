c4f6d2a1ca59ca1b6acb927acab98670baf2e5bf
Meta-Amit-Chhipa <99795085+Meta-Amit-Chhipa@users.noreply.github.com>
2022-04-11 11:16:34 +0530

    Update Details.js

--- src/Details.js
+++ src/Details.js
@@ -3,7 +3,6 @@ import React from "react";
 function PostDetail(props){
     return(
         <div>
-            <h3>{props.CurrentTitle}</h3>
            <p>{props.CurrentDescription}</p>
         </div>
     )
