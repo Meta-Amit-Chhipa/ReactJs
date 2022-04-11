1021a0c7159fe418237e5f53a23c22b8a41448b4
Meta-Amit-Chhipa <99795085+Meta-Amit-Chhipa@users.noreply.github.com>
2022-04-11 11:29:50 +0530

    Update Blogs.js

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
 import {FaRegThumbsUp,FaUserCircle} from "react-icons/fa";
+
 function Blogs(props){
     return(
-        <div className="container ">
-            <div className="Avatar">
-                <FaUserCircle style={{fontSize: '35px',color:'gray'}}/>
-            </div>
-            <h3 className="card-title">{props.title}</h3>
-            <p className="card-detail">{props.desc}</p>
-            <div className="Icon">
-            <FaRegThumbsUp style={{color: 'blue', fontSize: '20px'}}/>
-            </div>
+        <div>
+            {
+                props.item.map(Number=>(
+                   <div className="container ">
+                   <div className="Avatar">
+                  <FaUserCircle style={{fontSize: '35px',color:'gray'}}/>
+                   </div>
+                 <h3 className="card-title">{Number.title}</h3>
+                 <p className="card-detail">{Number.desc}</p>
+                 <div className="Icon">
+                 <FaRegThumbsUp style={{color: 'blue', fontSize: '20px'}}/>
+                </div>
+               </div>
+                 ))
+              }
         </div>
     )
 }
