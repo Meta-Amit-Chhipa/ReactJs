dcecfc8df6c47e0794db03ba5607bc920b938c80
Meta-Amit-Chhipa <99795085+Meta-Amit-Chhipa@users.noreply.github.com>
2022-04-12 14:10:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { Form } from './Components/Form';
-import { SampleForm } from './Components/SampleForm';
+
 function App() {
   return (
     <div>
