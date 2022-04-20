6958175c2b0aa60e45fc3037bb71726164586b05
Meta-Amit-Chhipa <99795085+Meta-Amit-Chhipa@users.noreply.github.com>
2022-04-20 23:46:06 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,54 +1,30 @@
 import React from 'react'
-import { MdOutlineMessage} from 'react-icons/md';
 import PostDetail from './Details';
 import PostList from './PostList';
 
-export class CreatePost extends React.Component{
-    constructor(props){
+export class CreatePost extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
-            CurrTab:0,
-            data:[
-                {id:'1',title:'Comment 1 Title',desc:"Description of Comment 1"},
-                {id:'2',title:'Comment 2 Title',desc:"Description of Comment 2"},
-                {id:'3',title:'Comment 3 Title',desc:"Description of Comment 3"},
-                {id:'4',title:'Comment 4 Title',desc:"Description of Comment 4"},
-                {id:'5',title:'Comment 5 Title',desc:"Description of Comment 5"},
+        this.state = {
+            CurrTab: 0,
+            data: [
+                { id: '1', title: 'Comment 1 Title', desc: "Description of Comment 1" },
+                { id: '2', title: 'Comment 2 Title', desc: "Description of Comment 2" },
+                { id: '3', title: 'Comment 3 Title', desc: "Description of Comment 3" },
+                { id: '4', title: 'Comment 4 Title', desc: "Description of Comment 4" },
+                { id: '5', title: 'Comment 5 Title', desc: "Description of Comment 5" },
             ]
         }
         this.ClickedTab = this.ClickedTab.bind(this);
     }
     ClickedTab(CurrTab) {
         this.setState({ CurrTab });
-      }
-    
-    render(){
-        // const element = <FontAwesomeIcon icon="fa-solid fa-message-captions" />
-        return(
-            <div className="MainBox">
-                <div className="title">
-                <h3>Comment Box  </h3>
-                </div>
-                <div className="tab">
-                    {
-                        this.state.data.map((button,i)=>(
-                            <button key={button.id} className="tablinks" onClick={()=>this.ClickedTab(i)}>
-                            Comment {button.id} </button>
-                        ))
-                    }
-                </div>
-                <div className="tabContent">
-                    {
-                        this.state.CurrTab!==-1 &&
-                        <>
-                         <div className="TitleIcons">
-                         <MdOutlineMessage style={{color:'red',marginLeft:'20px'}}/>
-                         </div>
-                          <PostList CurrentTitle={this.state.data[this.state.CurrTab].title} />
-                          <PostDetail CurrentDescription= {this.state.data[this.state.CurrTab].desc}/>
-                        </>
-                    }
-                </div>
+    }
+    render() {
+        return (
+            <div  className="MainBox">
+                <PostList data={this.state.data} CurrTab={this.state.CurrTab} ClickedTab={this.ClickedTab} />
+                <PostDetail data={this.state.data} CurrTab={this.state.CurrTab} />
             </div>
         )
     }
