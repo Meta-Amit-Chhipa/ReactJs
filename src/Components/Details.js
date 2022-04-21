c403c98bbf7200562ef34448133a3fef13411a68
Meta-Amit-Chhipa <99795085+Meta-Amit-Chhipa@users.noreply.github.com>
2022-04-21 18:06:11 +0530

    Update Details.js

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,32 +1,31 @@
 import React from "react";
 
-function Deatils(props){
-    return(
+function Details(props) {
+    return (
         <div className="container">
-            <br/><br/>
-           {
-               props.items.map(Number=>(
-                <div>
-                    {
-                    Number.title!=="" && Number.description!=="" ?
-                    <div className="card">
-                    <div className="card-body">
-                    <h5 className="card-title float-left">
-                    <span style={{ textDecoration:Number.completed ? "line-through" : "" }}>
-                    {Number.title}
-                    </span></h5>
-                    <p className="card-text">
-                    <span style={{ textDecoration:Number.completed ? "line-through" : "" }}>
-                    {Number.description}
-                    </span></p>
-                    <button onClick={()=>props.deleteItem(Number.id)} className="btn btn-danger float-right">X</button>
+            <br /><br />
+            {
+                props.items.map(Number => (
+                    <div>
+                        {
+                            Number.title !== "" && Number.description !== "" ?
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h5 className="card-title float-left">
+                                            <span style={{ textDecoration: Number.completed ? "line-through" : "" }}>
+                                                {Number.title}
+                                            </span></h5>
+                                        <p className="card-text">
+                                            <span style={{ textDecoration: Number.completed ? "line-through" : "" }}>
+                                                {Number.description}
+                                            </span></p>
+                                        </div>
+                                </div> : null
+                        }
                     </div>
-                </div>:null
-                }
-                </div>
-               ))
-           }
+                ))
+            }
         </div>
     )
 }
-export default Deatils;
+export default Details;
